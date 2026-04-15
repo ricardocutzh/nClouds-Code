d52d6c7bbff1fc2447125b70f4eb213bd85b6bd1
ricardocutzh <ricardo@nclouds.com>
2026-04-14 19:19:49 -0600

    Updating tables for shows

--- ABFilms/Infrastructure/abfilms-lambdas/src/show_data_save/main.py
+++ ABFilms/Infrastructure/abfilms-lambdas/src/show_data_save/main.py
@@ -19,6 +19,24 @@ headers = {
     "Prefer": "return=representation"
 }
 
+def get_video(data, series_title, episode_number, episode_title):
+    video_id = video_exists(series_title, episode_number, episode_title)
+    if video_id == -1:
+        return save_video(data)["id"]
+    return video_id
+
+def get_series(data, series_title):
+    serie_id = series_exist(series_title)
+    if serie_id == -1:
+        return save_series(data)["id"]
+    return serie_id
+
+def get_series_episode(data, serie_id, video_id):
+    episode_id = episode_exists(serie_id, video_id)
+    if episode_id == -1:
+        return save_episode(data, serie_id, video_id)["id"]
+    return episode_id
+
 def drm_enabled():
     if str(DRM_ENABLED) == "True":
         return True
@@ -143,7 +161,7 @@ def save_video(data):
         "title": f"{data["Original_CSV_Data"]["Movie/Show Title"]} - Episode {data["Original_CSV_Data"]["Episode Number"]} - {data["Original_CSV_Data"]["Episode Name"]}",
         "description": f"{data["Original_CSV_Data"]["Episode Synopsis"]}",
         "duration": int(data["Original_CSV_Data"]["Episode Running Time"]),
-        #"category": f"{data["Original_CSV_Data"]["Genre"]}",
+        "category": f"{data["Original_CSV_Data"]["Genre"]}",
         "thumbnail": f"{str(SHOWS_PUBLIC_CLOUDFRONT_URL)}/s3_key/img.jpg",
         "video_url": f"{data["UserMetadata"]["MasterFileURL"]}",
         "drm_protected": drm_enabled(),
@@ -177,28 +195,26 @@ def save_video(data):
         logger.error(f"-- Function save_video:  Error saving ing Supabase: {e}")
         raise Exception(f"-- Function save_video:  Error saving ing Supabase: {e}")
         
-
 def save_series(data):
     # save the series and return id
+    rating = "PG" if str(data["Original_CSV_Data"]["Rating"]) == "" else str(data["Original_CSV_Data"]["Rating"])
     series_data = {
         "title": f"{data["Original_CSV_Data"]["Movie/Show Title"]}",
         "description": f"{data["Original_CSV_Data"]["Episode Synopsis"]}",
+        "category": f"{data["Original_CSV_Data"]["Genre"]}",
         "poster_url": f"{str(SHOWS_PUBLIC_CLOUDFRONT_URL)}/s3_key/img.jpg",
         "trailer_url": f"{str(SHOWS_PUBLIC_CLOUDFRONT_URL)}/s3_key/img.mp4",
         "director": str(data["Original_CSV_Data"]["Director(s)"]),
         "release_year": int(data["Original_CSV_Data"]["Production Year"]),
-        "rating":  f"{data["Original_CSV_Data"]["Rating"]}",
+        "rating":  str(rating),
         "is_premium": True,
         "featured": True, # need to ask the value for this
-        "status": "upcoming",
-        "total_seasons": 0, # first time it is created
-        "total_episodes": 0 # first time it is created
         "genre": f"{data["Original_CSV_Data"]["Genre"]}",
         "is_rentable": True,
-        "rental_price": 4.99
+        "review_status": "pending"
     }
     try:
-        response = requests.post(f"{SUPABASE_URL}/series", headers=headers, json=video_data)
+        response = requests.post(f"{SUPABASE_URL}/series", headers=headers, json=series_data)
         
         response.raise_for_status()
         
@@ -210,10 +226,171 @@ def save_series(data):
         logger.error(f"-- Function save_series:  Error saving ing Supabase: {e}")
         raise Exception(f"-- Function save_series:  Error saving ing Supabase: {e}")
 
-def save_episode(data):
+def save_episode(data, serie_id, video_id):
     # save the episode 
-    pass
+    episode_data = {
+        "series_id": str(serie_id),
+        "video_id": str(video_id),
+        "season_number": int(data["Original_CSV_Data"]["Season Number"]),
+        "episode_number": int(data["Original_CSV_Data"]["Episode Number"]),
+        "episode_title": data["Original_CSV_Data"]["Episode Name"]
+    }
+    try:
+        response = requests.post(f"{SUPABASE_URL}/series_episodes", headers=headers, json=episode_data)
+        
+        response.raise_for_status()
+        
+        result = response.json()
 
+        return result[0] if isinstance(result, list) else result
+
+    except Exception as e:
+        logger.error(f"-- Function save_episode:  Error saving ing Supabase: {e}")
+        raise Exception(f"-- Function save_episode:  Error saving ing Supabase: {e}")
+
+def update_series(data, series_id):
+    rating = "PG" if str(data["Original_CSV_Data"]["Rating"]) == "" else str(data["Original_CSV_Data"]["Rating"])
+    series_data = {
+        "title": f"{data["Original_CSV_Data"]["Movie/Show Title"]}",
+        "description": f"{data["Original_CSV_Data"]["Episode Synopsis"]}",
+        "category": f"{data["Original_CSV_Data"]["Genre"]}",
+        "poster_url": f"{str(SHOWS_PUBLIC_CLOUDFRONT_URL)}/s3_key/img.jpg",
+        "trailer_url": f"{str(SHOWS_PUBLIC_CLOUDFRONT_URL)}/s3_key/img.mp4",
+        "director": str(data["Original_CSV_Data"]["Director(s)"]),
+        "release_year": int(data["Original_CSV_Data"]["Production Year"]),
+        "rating":  str(rating),
+        "is_premium": True,
+        "featured": True, # need to ask the value for this
+        "genre": f"{data["Original_CSV_Data"]["Genre"]}",
+        "is_rentable": True,
+        "review_status": "pending",
+        "total_seasons": get_total_seasons(data, series_id),
+        "total_episodes": get_total_episodes(data, series_id)
+    }
+    try:
+        response = requests.patch(f"{SUPABASE_URL}/series?id=eq.{series_id}", headers=headers, json=series_data)
+        
+        response.raise_for_status()
+        
+        result = response.json()
+
+        return result[0] if isinstance(result, list) else result
+
+    except Exception as e:
+        logger.error(f"-- Function update_series:  Error saving ing Supabase: {e}")
+        raise Exception(f"-- Function update_series:  Error saving ing Supabase: {e}")
+
+def update_videos(data, video_id):
+    video_data = {
+        "title": f"{data["Original_CSV_Data"]["Movie/Show Title"]} - Episode {data["Original_CSV_Data"]["Episode Number"]} - {data["Original_CSV_Data"]["Episode Name"]}",
+        "description": f"{data["Original_CSV_Data"]["Episode Synopsis"]}",
+        "duration": int(data["Original_CSV_Data"]["Episode Running Time"]),
+        "category": f"{data["Original_CSV_Data"]["Genre"]}",
+        "thumbnail": f"{str(SHOWS_PUBLIC_CLOUDFRONT_URL)}/s3_key/img.jpg",
+        "video_url": f"{data["UserMetadata"]["MasterFileURL"]}",
+        "drm_protected": drm_enabled(),
+        "drm_license_url": str(DRM_LICENCE_URL),
+        "featured": True, # need to ask the value for this
+        "release_year": int(data["Original_CSV_Data"]["Production Year"]),
+        "rating":  f"{data["Original_CSV_Data"]["Rating"]}",
+        "cast_members": str(data["Original_CSV_Data"]["Cast"]).split(","),
+        "director": str(data["Original_CSV_Data"]["Director(s)"]),
+        "trailer_url": f"{str(SHOWS_PUBLIC_CLOUDFRONT_URL)}/s3_key/img.mp4",
+        "subscription_required": "premium",
+        "is_premium": True,
+        "quality": "HD",
+        "producer": str(data["Original_CSV_Data"]["Producer(s)"]),
+        "production_company": str(data["Original_CSV_Data"]["Studio"]),
+        "genre": f"{data["Original_CSV_Data"]["Genre"]}",
+        "is_rentable": True,
+        "rental_price": 4.99
+    }
+    try:
+        response = requests.patch(f"{SUPABASE_URL}/videos?id=eq.{video_id}", headers=headers, json=video_data)
+        
+        response.raise_for_status()
+        
+        result = response.json()
+
+        return result[0] if isinstance(result, list) else result
+
+    except Exception as e:
+        logger.error(f"-- Function update_videos:  Error saving ing Supabase: {e}")
+        raise Exception(f"-- Function update_videos:  Error saving ing Supabase: {e}")
+
+def update_episodes(data, serie_id, video_id):
+    params = {
+        "video_id": f"eq.{video_id}",
+        "series_id": f"eq.{serie_id}"
+    }
+    episode_data = {
+        "season_number": int(data["Original_CSV_Data"]["Season Number"]),
+        "episode_number": int(data["Original_CSV_Data"]["Episode Number"]),
+        "episode_title": data["Original_CSV_Data"]["Episode Name"]
+    }
+    try:
+        response = requests.patch(f"{SUPABASE_URL}/series_episodes", headers=headers, params=params, json=episode_data)
+        
+        response.raise_for_status()
+        
+        result = response.json()
+
+        return result[0] if isinstance(result, list) else result
+
+    except Exception as e:
+        logger.error(f"-- Function update_episodes:  Error saving ing Supabase: {e}")
+        raise Exception(f"-- Function update_episodes:  Error saving ing Supabase: {e}")
+
+def get_total_seasons(event, serie_id):
+    url = f"{SUPABASE_URL}/series_episodes"
+    
+    # URL Parameters:
+    # 1. Filter by series_id
+    # 2. Select only the season_number column
+    # 3. Order by season_number in descending order (desc)
+    params = {
+        "series_id": f"eq.{serie_id}",
+        "select": "season_number",
+        "order": "season_number.desc"
+    }
+
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+        data = response.json()
+
+        ordered_seasons = list(dict.fromkeys([item['season_number'] for item in data]))
+        
+        return len(ordered_seasons)
+
+    except Exception as e:
+        logger.error(f"-- Function get_latest_season:  Error saving ing Supabase: {e}")
+        raise Exception(f"-- Function get_latest_season:  Error saving ing Supabase: {e}")
+
+def get_total_episodes(event, serie_id):
+    url = f"{SUPABASE_URL}/series_episodes"
+    
+    # URL Parameters:
+    # 1. Filter by series_id
+    # 2. Select only the season_number column
+    # 3. Order by season_number in descending order (desc)
+    params = {
+        "series_id": f"eq.{serie_id}",
+        "select": "episode_number"
+    }
+
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+        data = response.json()
+
+        episodes = list(dict.fromkeys([item['episode_number'] for item in data]))
+        
+        return len(episodes)
+
+    except Exception as e:
+        logger.error(f"-- Function get_latest_season:  Error saving ing Supabase: {e}")
+        raise Exception(f"-- Function get_latest_season:  Error saving ing Supabase: {e}")
 
 def lambda_handler(event, context):
     # 1. Print the event as a formatted JSON string
@@ -221,16 +398,23 @@ def lambda_handler(event, context):
     logger.info(json.dumps(event))
 
     try:
-        # series_id = series_exist("Buster Beaver")
-        # logger.info(f"series id: {series_id}")
+        video_id = get_video(event, event["Original_CSV_Data"]["Movie/Show Title"], event["Original_CSV_Data"]["Episode Number"], event["Original_CSV_Data"]["Episode Name"])
+        logger.info(f"-- video_id: {video_id}")
+        series_id = get_series(event, event["Original_CSV_Data"]["Movie/Show Title"])
+        logger.info(f"-- series_id: {series_id}")
+        episode_id = get_series_episode(event, series_id, video_id)
+        logger.info(f"-- episode_id: {episode_id} with video_id {video_id} and series_id {series_id}")
 
-        # video_id = video_exists("Buster Beaver", 6, "Love At First Fright")
-        # logger.info(f"video id: {video_id}")
+        logger.info("-- Updating information required...")
 
-        # series_episode_id = episode_exists(series_id, video_id)
-        # logger.info(f"series_episode id: {series_episode_id}")
-        # return event        
-        save_video(event)
+        logger.info(json.dumps(update_episodes(event, series_id, video_id)))
+        logger.info(json.dumps(update_videos(event, video_id)))
+        logger.info(json.dumps(update_series(event, series_id)))
+
+        logger.info("-- Finish All Updates")
+        
+        return event
+
     except Exception as e:
         logger.error(f">> Function: lambda_handler: {str(e)}")
         raise Exception(f">> Function: lambda_handler: {str(e)}")
